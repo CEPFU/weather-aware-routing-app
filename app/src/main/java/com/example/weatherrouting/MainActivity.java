ecd5c60c0ddb3ada9f21621e91e5f2d97339bee0
Henning Steinke <henning.steinke@fu-berlin.de>
2016-06-09 15:14:48 +0200

    Adds basic Mapbox view

--- app/src/main/java/com/example/weatherrouting/MainActivity.java
+++ app/src/main/java/com/example/weatherrouting/MainActivity.java
@@ -3,11 +3,50 @@ package com.example.weatherrouting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mapbox.mapboxsdk.maps.MapView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MapView _mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _mapView = (MapView) findViewById(R.id.mapview);
+        if (_mapView != null) {
+            _mapView.onCreate(savedInstanceState);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        _mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        _mapView.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        _mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        _mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        _mapView.onDestroy();
     }
 }
